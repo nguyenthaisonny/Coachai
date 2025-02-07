27690b2515614814668f20b34756df3274766e62
Nguyễn Thái Son Ny <134358623+nguyenthaisonny@users.noreply.github.com>
2025-02-07 18:40:21 +1100

    fix bug check User onborading

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { checkUserLogin, getUserOnBoardingStatus } from '@/actions/user';
 import { redirect } from 'next/navigation';
-import React from 'react';
+import React, { use } from 'react';
 import DashBoardView from './_components/dashboard-view';
 import { getIndustryInsights } from '@/actions/dashboard';
+import { checkUser } from '@/lib/checkUser';
 
 const IndustryInsightPage = async () => {
+  const user = await checkUser();
+  if(!user) throw new Error('Fail to check auth user')
   const { isOnboarded } = await getUserOnBoardingStatus();
   if (!isOnboarded) {
     redirect('/onboarding');
